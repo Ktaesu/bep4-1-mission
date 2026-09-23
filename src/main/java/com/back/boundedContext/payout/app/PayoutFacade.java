dcd44a56f1399f94789c7cb91ec0da047674ab88
Ktaesu <tlsdnjsehd99@naver.com>
2026-09-23 12:04:10 +0900

    0048

--- src/main/java/com/back/boundedContext/payout/app/PayoutFacade.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutFacade.java
@@ -19,7 +19,9 @@ public class PayoutFacade {
     private final PayoutCreatePayoutUseCase payoutCreatePayoutUseCase;
     private final PayoutAddPayoutCandidateItemsUseCase payoutAddPayoutCandidateItemsUseCase;
     private final PayoutCollectPayoutItemsMoreUseCase payoutCollectPayoutItemsMoreUseCase;
+    private final PayoutCompletePayoutsMoreUseCase payoutCompletePayoutsMoreUseCase;
     private final PayoutSupport payoutSupport;
+
 
     @Transactional
     public void syncMember(MemberDto member) {
@@ -27,8 +29,8 @@ public class PayoutFacade {
     }
 
     @Transactional
-    public Payout createPayout(PayoutMemberDto payee) {
-        return payoutCreatePayoutUseCase.createPayout(payee);
+    public Payout createPayout(int payeeId) {
+        return payoutCreatePayoutUseCase.createPayout(payeeId);
     }
 
     @Transactional
@@ -45,5 +47,10 @@ public class PayoutFacade {
     public List<PayoutCandidateItem> findPayoutCandidateItems() {
         return payoutSupport
                 .findPayoutCandidateItems();
+    }
+
+    @Transactional
+    public RsData<Integer> completePayoutsMore(int limit) {
+        return payoutCompletePayoutsMoreUseCase.completePayoutsMore(limit);
     }
 }
